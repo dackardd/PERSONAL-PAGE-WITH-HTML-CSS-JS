b332ef90ee5762988e244f44d3d6e71070ba0633
dackardd <100649636+dackardd@users.noreply.github.com>
2022-06-03 13:11:21 +0300

    Personal Page Added

---
+++ Personal Page Project/JS/app.js
@@ -0,0 +1,20 @@
+//SLİDER
+
+// var myIndex = 0;
+// carousel();
+
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";  
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {myIndex = 1}    
+//   x[myIndex-1].style.display = "block";  
+//   setTimeout(carousel, 3000); // 3 Saniyede bir slider otomatik değişim.
+// }
+// (function($){
+//   new WOW().init();
+// })(jQuery);
+
